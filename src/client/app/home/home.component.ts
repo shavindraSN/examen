ecd9c0b334009a2f2eb1134377f5242b6ec23175
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-09-16 10:36:37 +0530

    Remove unnecessary files and references

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { NameListService } from '../shared/index';
+import { Component, AfterViewInit } from '@angular/core';
 
 /**
  * This class represents the lazy loaded HomeComponent.
@@ -8,53 +7,9 @@ import { NameListService } from '../shared/index';
   moduleId: module.id,
   selector: 'sd-home',
   templateUrl: 'home.component.html',
-  styleUrls: ['home.component.css'],
 })
-export class HomeComponent implements OnInit, AfterViewInit {
-
+export class HomeComponent implements AfterViewInit {
   ngAfterViewInit() {
     $.material.init();
   }
-
-  newName: string = '';
-  errorMessage: string;
-  names: any[] = [];
-
-  /**
-   * Creates an instance of the HomeComponent with the injected
-   * NameListService.
-   *
-   * @param {NameListService} nameListService - The injected NameListService.
-   */
-  constructor(public nameListService: NameListService) {}
-
-  /**
-   * Get the names OnInit
-   */
-  ngOnInit() {
-    this.getNames();
-  }
-
-  /**
-   * Handle the nameListService observable
-   */
-  getNames() {
-    this.nameListService.get()
-		     .subscribe(
-		       names => this.names = names,
-		       error =>  this.errorMessage = <any>error
-		       );
-  }
-
-  /**
-   * Pushes a new name onto the names array
-   * @return {boolean} false to prevent default form submit behavior to refresh the page.
-   */
-  addName(): boolean {
-    // TODO: implement nameListService.post
-    this.names.push(this.newName);
-    this.newName = '';
-    return false;
-  }
-
 }
