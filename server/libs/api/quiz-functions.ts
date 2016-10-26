da6223326f5e28c45c08553f250d101a429262d2
Gunaratna <praveengunaratna@yahoo.com>
2016-10-26 19:02:35 +0530

    Alter time based question loading algorithm -Take user desired time and question json as parameters

--- server/libs/api/quiz-functions.ts
+++ server/libs/api/quiz-functions.ts
@@ -2,36 +2,43 @@
 import * as shuffleArray from "shuffle-array";
 
 export class QuizFunctions {
+    
     count: number;
     temp = 0;
     all = new Array();
     all2 = new Array();
+    quizeTime:number;
 
-    constructor(jsonval) {
+    /**
+     * Counstructor  initialize base variables
+     * @param {object} questionJson - Rows return from the query
+     * @param {object} quizeTime - quize time passed by the user
+     */ 
+    constructor(quizeTime:number, questionJson) {
         let question = new Array();
         let time = new Array();
-
-        for (var i = 0; i < jsonval.data.length; i++) {
-            question.push(jsonval.data[i].id);
-            time.push(jsonval.data[i].question_time);
+        this.quizeTime=quizeTime;
+        for (var i = 0; i < questionJson.data.length; i++) {
+            question.push(questionJson.data[i].id);
+            time.push(questionJson.data[i].question_time);
             this.all.push([question[i], time[i]]);
         }
     }
 
+    /**
+     * Method  make a random array
+     * @return {object} all -json object consists of qestion id
+     */ 
     makeQuestionRandomArray() {
-        shuffleArray(this.all);
-       // console.log(this.all);
-    }
-    
-    makeTime() {
         let question = new Array();
         let time = new Array();
         this.count = 0;
 
+        shuffleArray(this.all);
         for (var i = 0; i < this.all.length; i++) {
             this.temp = parseInt(this.all[i][1]);
 
-            if (this.count + this.temp < 15) {
+            if (this.count + this.temp < this.quizeTime) {
                 this.count = this.count + this.temp;
                 question.push(this.all[i]);
                 time.push(this.all[i][1]);
@@ -41,3 +48,4 @@ export class QuizFunctions {
         return this.all2;
     }
 }
+
