c94612aa3fc4016aa33e196f926e2b53ee148b1c
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-10-29 13:15:29 +0530

    Update user functions with following helpers - Get user by ID - Update user details

--- server/libs/api/user-functions.ts
+++ server/libs/api/user-functions.ts
@@ -16,11 +16,57 @@ export class UserFunctions {
      * @param {mysql.IConnection} connection - Connection object that has connection metadata
      * @param {sendResponse} callback - Response rows as JSON to the request
      * 
-     */ 
+     */
     getAllUsers(connection: mysql.IConnection, callback) {
         let query = 'SELECT * FROM users';
         connection.query(query, (err, rows) => {
             callback(rows);
         });
+    }
+
+    /**
+     * Callback for responding with specified user details that query from the Database as a JSON object
+     * 
+     * @callback sendResponse
+     * @param {object} row - Row return from the query
+     */
+
+    /**
+     * Get speficied user details from the database. 
+     * 
+     * @param {number} userID - ID of the use to retrieve data
+     * @param {mysql.IConnection} connection - Connection object that has connection metadata
+     * @param {sendResponse} callback - Response rows as JSON to the request
+     * 
+     */
+    getUser(userID: number, connection: mysql.IConnection, callback) {
+        let query = 'SELECT * FROM users WHERE id=' + userID.toString();
+        connection.query(query, (err, row) => {
+            callback(row);
+        })
+    }
+
+    /**
+     * Callback for responding with specified user details that query from the Database as a JSON object
+     * 
+     * @callback sendResponse
+     * @param {object} row - Row return from the query
+     */
+
+    /**
+     * Get speficied user details from the database. 
+     * 
+     * @param {number} userID - ID of the use to retrieve data
+     * @param {string} userName - New User name to be updated with
+     * @param {mysql.IConnection} connection - Connection object that has connection metadata
+     * @param {sendResponse} callback - Response rows as JSON to the request
+     * 
+     */
+    
+    updateUser(userID: number, userName:string, connection: mysql.IConnection, callback) {
+        let query = 'UPDATE users SET userName= WHERE id=' + userID.toString();
+        connection.query(query, (err, row) => {
+            callback(row);
+        })
     }
 }
