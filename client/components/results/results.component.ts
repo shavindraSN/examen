08a12b8f6eb93035c99a97a74958706b3e67710b
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-11-04 10:58:59 +0530

    Add Result component, view and service boilerplate

---
+++ client/components/results/results.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'results',
+    templateUrl: 'client/results/results.components.html'
+})
+export class ResultsComponent { }
