7dc3c4843ce418c9dcc8872562f86eabfd98248b
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-11-19 10:49:14 +0530

    Add routes to addquestionscomponent

--- client/components/home/home.routing.ts
+++ client/components/home/home.routing.ts
@@ -5,13 +5,15 @@ import { InitModelPaperComponent } from '../initModelPaper/init-model-paper.comp
 import { InitQuizPaperComponent } from '../initQuizPaper/init-quiz-paper.component';
 import { PaperSummaryComponent } from '../paperSummary/paper-summary.component';
 import { PaperComponent } from '../paper/paper.component';
+import { AddQuestionsComponent } from '../uploadQuestions/addQuestions/add-questions.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'initmodelpaper', component: InitModelPaperComponent },
     { path: 'initquizpaper', component: InitQuizPaperComponent },
     { path: 'papersummary', component: PaperSummaryComponent },
-    { path: 'initquizpaper/paper/:subid/:time', component: PaperComponent}
+    { path: 'initquizpaper/paper/:subid/:time', component: PaperComponent},
+    { path: 'addquestions', component:AddQuestionsComponent }
 ];
 
 export const routing = RouterModule.forChild(routes);
