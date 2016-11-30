5a5964a47b94793fb17457c3dd055c0bb8808568
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-11-30 10:11:52 +0530

    Change routes to accept first name and user id

--- client/routes.ts
+++ client/routes.ts
@@ -2,11 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { VerifyEmailComponent } from './components/verifyEmail/verify-email.component'
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'login',  component: LoginComponent},
-    { path: 'registration', component: RegistrationComponent}
+    { path: 'registration', component: RegistrationComponent},
+    { path: 'verifyEmail/:fname/:id', component:VerifyEmailComponent }
 ];
-
 export const routing = RouterModule.forRoot(routes, { useHash: true });
