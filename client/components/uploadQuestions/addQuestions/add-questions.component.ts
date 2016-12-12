293d21b765b19521f4b2f825d748866be622fe2b
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-12-12 11:11:10 +0530

    Add add-question basic form structure

--- client/components/uploadQuestions/addQuestions/add-questions.component.ts
+++ client/components/uploadQuestions/addQuestions/add-questions.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { NotificationsService, SimpleNotificationsComponent } from 'angular2-notifications';
 
 @Component({
     selector: 'add-questions',
     templateUrl: 'client/components/uploadQuestions/addQuestions/add-questions.component.html'
 })
-export class AddQuestionsComponent {}
+export class AddQuestionsComponent {
+    options = {
+        position: ['top', 'right'],
+        timeOut: 3000,
+        lastOnBottom: true,
+        clickToClose: true
+    };
+
+    constructor(
+        private _notification: NotificationsService
+    ) { }
+}
