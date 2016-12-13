0f09820920225d8f0302cae7d45d05b22628962e
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-12-13 17:37:36 +0530

    Change following  - Automatically redirect to login when user is not authenticated  - Add Redirecting message when it take long time

--- client/components/home/home.component.ts
+++ client/components/home/home.component.ts
@@ -1,7 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from '../../service/auth.service'
 
 @Component({
     selector: "home",
-    templateUrl: 'client/components/home/home.component.html'
+    templateUrl: 'client/components/home/home.component.html',
+    providers: [AuthService]
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+    authenticate = false;
+    constructor(private _authService: AuthService) {}
+    ngOnInit() {
+        if(!localStorage.getItem('auth')) {
+            this._authService.getCredentials(() => {
+                this.authenticate = true;
+            });
+        }
+        else {
+            this.authenticate = this._authService.checkCredentials();
+        }
+    }
+}
