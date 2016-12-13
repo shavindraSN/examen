54d8254a9731b4d971db6f91c9d820193a3e46b2
sumeera shakila <sumeera shakila>
2016-12-13 11:47:28 +0530

    Add Question uploading user interface  - This include district fetching

--- client/components/uploadQuestions/addQuestions/add-questions.component.ts
+++ client/components/uploadQuestions/addQuestions/add-questions.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NotificationsService, SimpleNotificationsComponent } from 'angular2-notifications';
+import { districtList, District } from '../../registration/districts';
 
 @Component({
     selector: 'add-questions',
     templateUrl: 'client/components/uploadQuestions/addQuestions/add-questions.component.html'
 })
 export class AddQuestionsComponent {
+    districts = districtList;
     options = {
         position: ['top', 'right'],
         timeOut: 3000,
