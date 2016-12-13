682292dc49ac8e89708534a5e3ddf6cd3fc74e9a
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-12-13 17:38:10 +0530

    Add auto redirection and messages when user is not authenticated

--- client/components/initQuizPaper/init-quiz-paper.component.ts
+++ client/components/initQuizPaper/init-quiz-paper.component.ts
@@ -2,20 +2,26 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { InitQuizPaperService } from './init-quiz-paper.service';
 import { Subject } from '../../sharedClasses/subject';
 import { PaperComponent } from '../paper/paper.component';
+import { AuthService } from '../../service/auth.service'
 import { Router } from '@angular/router';
 
 @Component({
     selector: 'quiz-paper',
     templateUrl: 'client/components/initQuizPaper/init-quiz-paper.component.html',
-    providers: [InitQuizPaperService]
+    providers: [InitQuizPaperService, AuthService]
 })
 export class InitQuizPaperComponent implements OnInit {
 
     subjects: Subject[];
     selectedSubject: Subject;
     submitted: boolean;
+    authenticate = false;
     
-    constructor(private initQuizPaperService: InitQuizPaperService, private _router: Router) { }
+    constructor(
+        private initQuizPaperService: InitQuizPaperService, 
+        private _router: Router,
+        private _authService: AuthService
+    ) { }
 
     getSubjects(): void {
         this.initQuizPaperService.getSubjects().then(subjects => this.subjects = subjects);
@@ -25,6 +31,14 @@ export class InitQuizPaperComponent implements OnInit {
         this.selectedSubject = subject;
     }
     ngOnInit() {
+        if(!localStorage.getItem('auth')) {
+            this._authService.getCredentials(() => {
+                this.authenticate = true;
+            });
+        }
+        else {
+            this.authenticate = this._authService.checkCredentials();
+        }
         this.getSubjects();
     }
     onclick(){
