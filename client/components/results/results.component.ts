7528d641661c0066a90fc421e387ec39502afe46
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-12-14 23:15:32 +0530

    Add Result displaying component  - This will get result details from paper component and display

--- client/components/results/results.component.ts
+++ client/components/results/results.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router'
+import { AuthService } from '../../service/auth.service';
+
 
 @Component({
     selector: 'results',
-    templateUrl: 'client/results/results.components.html'
+    templateUrl: 'client/components/results/results.component.html',
+    providers: [AuthService]
 })
-export class ResultsComponent { }
+export class ResultsComponent implements OnInit {
+    authenticate = true;
+    correctAnswer: number;
+    subjectID: number;
+    totalQuestion: number;
+    percentage: number;
+
+    constructor(
+        private _authService: AuthService,
+        private _router: Router,
+        private _route: ActivatedRoute
+    ) { }
+
+    ngOnInit() {
+        this._authService.getUserDetails()
+            .then(data => {})
+        this._route.params.forEach((params: Params) => {
+            this.correctAnswer = +params['correct'];
+            this.subjectID = +params['subid'];
+            this.totalQuestion = +params['total'];
+        });
+        this.percentage = +((this.correctAnswer / this.totalQuestion) * 100).toFixed(2);
+    }
+}
--- client/components/results/results.service.ts
+++ client/components/results/results.service.ts
@@ -1,4 +1,8 @@
 import { Injectable }  from '@angular/core';
 
 @Injectable()
-export class ResultsService { }
+export class ResultsService { 
+    getSubject(id) {
+        
+    }
+}
